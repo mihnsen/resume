f560e1073b24afbdf81f0eff3fa4899601c8b660
minhnt <minhnt@ownego.com>
2015-11-02 10:46:28 +0700

    Initital structure

---
+++ src/assets/js/oe.main.js
@@ -0,0 +1,19 @@
+(function(window, $) {
+  var $doc = $(document);
+  var $body = $('body');
+  var $grand = $('body, html');
+
+  var config = {
+  }
+
+  // Common functions
+  var oe = {
+  };
+
+  // Make it global
+  window.oe = oe;
+})(window, jQuery);
+
+$(function() {
+  
+});
